18d9947bfd4d43b319e5c005ad4446e4c4c3a13c
Rohit <rohitnair987@gmail.com>
2017-12-10 17:59:07 -0500

    Write text and datTime to a local file storage

--- ExtendedMemory/Views/HomePage.xaml.cs
+++ ExtendedMemory/Views/HomePage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 using Xamarin.Forms;
 
 namespace ExtendedMemory.Views
@@ -16,6 +16,16 @@ namespace ExtendedMemory.Views
             Button button = (Button)sender;
 
             lblDisplay.Text = txtEntry.Text;
+
+            // /Users/rohit/Library/Developer/CoreSimulator/Devices/45163EF8-B7EB-4321-877C-F8CAA9B5D484/data/Containers/Data/Application/77BFD79E-8A35-436C-B8F2-1E5CB1D047B1/Documents
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string filePath = Path.Combine(path, "out.txt");
+            File.AppendAllText(filePath, $"{txtEntry.Text}#{DateTime.Now}#\n");
+
+            //testing start
+            string text = File.ReadAllText(filePath);
+            Console.WriteLine(text);
+            //testing end
         }
     }
 }
