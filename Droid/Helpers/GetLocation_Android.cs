aad331b4fb89497e1bce4f6bfe3e00af0eaf1481
Rohit <rohitnair987@gmail.com>
2017-12-14 05:35:08 -0500

    Added skeleton code for getting the user's location

---
+++ Droid/Helpers/GetLocation_Android.cs
@@ -0,0 +1,20 @@
+﻿using ExtendedMemory.Models;
+using ExtendedMemory.Helpers;
+using ExtendedMemory.Android.Helpers;
+
+[assembly: Xamarin.Forms.Dependency(typeof(GetLocation_Android))]
+namespace ExtendedMemory.Android.Helpers
+{
+    public class GetLocation_Android : IGetLocation
+    {
+        public GetLocation_Android() {}
+
+        public Location GetUserLocation()
+        {
+            return new Location
+            {
+                City = "Android Bloo"
+            };
+        }
+    }
+}
---
+++ ExtendedMemory/Models/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtendedMemory.Models
+{
+    public class Location
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
+
