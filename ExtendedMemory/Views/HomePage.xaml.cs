4d0519554a752b7ee6988901d87b8cb27cf3812a
Rohit <rohitnair987@gmail.com>
2017-12-21 00:13:29 -0500

    got the reverse with hard-coded values

--- ExtendedMemory/Views/HomePage.xaml.cs
+++ ExtendedMemory/Views/HomePage.xaml.cs
@@ -3,62 +3,79 @@ using System;
 using System.IO;
 using Xamarin.Forms;
 using ExtendedMemory.Helpers;
+using Xamarin.Forms.Maps;
 
 namespace ExtendedMemory.Views
 {
     public partial class HomePage : ContentPage
     {
         Location userLocation;
+        Geocoder geoCoder;
 
         public HomePage()
         {
             InitializeComponent();
             time.Time = DateTime.Now.TimeOfDay;
-            userLocation = DependencyService.Get<IGetLocation>().GetUserLocation();
+            geoCoder = new Geocoder();
         }
 
         async void OnButtonClicked(object sender, EventArgs args)
         {
-            Button button = (Button)sender;
-
-            if (String.IsNullOrWhiteSpace(txtEntry.Text))
+            try
             {
-                await DisplayAlert("Enter Text", "Please enter some text", "OK");
-                return;
+                Button button = (Button)sender;
+
+                if (String.IsNullOrWhiteSpace(txtEntry.Text))
+                {
+                    await DisplayAlert("Enter Text", "Please enter some text", "OK");
+                    return;
+                }
+
+                userLocation = DependencyService.Get<IGetLocation>().GetUserLocation();
+
+                var position = new Position(39, -86);
+                Xamarin.FormsMaps.Init();
+                var possibleAddresses = await geoCoder.GetAddressesForPositionAsync(position);
+                foreach (var address in possibleAddresses)
+                {
+                    Console.WriteLine(address);
+                    txtEntry.Text = "boo " + address;
+                }
+
+                Console.WriteLine(userLocation.City);
+
+                //Location userLocation = IGetLocation();
+
+                //if (!CLLocationManager.LocationServicesEnabled)
+                //{
+                //    await DisplayAlert("Enable Location", "", "OK");
+                //    return;
+                //}
+
+                //var locationManager = new CLLocationManager();
+                //Console.WriteLine(locationManager.Location.Coordinate);
+
+                /*var position = new Position(locationManager.Location.Coordinate.Latitude, locationManager.Location.Coordinate.Longitude);
+                var possibleAddresses = await geoCoder.GetAddressesForPositionAsync(position);
+                foreach (var address in possibleAddresses)
+                {
+                    Console.WriteLine(address);
+                }*/
+
+
+
+                // /Users/rohit/Library/Developer/CoreSimulator/Devices/45163EF8-B7EB-4321-877C-F8CAA9B5D484/data/Containers/Data/Application/77BFD79E-8A35-436C-B8F2-1E5CB1D047B1/Documents
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                string filePath = Path.Combine(path, "out.txt");
+
+                File.AppendAllText(filePath, $"{txtEntry.Text}#{date.Date + time.Time}\n");
+
+
+                //string text = File.ReadAllText(filePath);
+                //Console.WriteLine(text);
+            }catch(Exception e){
+                Console.WriteLine(e);
             }
-
-            Console.WriteLine(userLocation.City);
-
-            //Location userLocation = IGetLocation();
-
-            //if (!CLLocationManager.LocationServicesEnabled)
-            //{
-            //    await DisplayAlert("Enable Location", "", "OK");
-            //    return;
-            //}
-
-            //var locationManager = new CLLocationManager();
-            //Console.WriteLine(locationManager.Location.Coordinate);
-
-            /*var position = new Position(locationManager.Location.Coordinate.Latitude, locationManager.Location.Coordinate.Longitude);
-            var possibleAddresses = await geoCoder.GetAddressesForPositionAsync(position);
-            foreach (var address in possibleAddresses)
-            {
-                Console.WriteLine(address);
-            }*/
-
-
-
-            // /Users/rohit/Library/Developer/CoreSimulator/Devices/45163EF8-B7EB-4321-877C-F8CAA9B5D484/data/Containers/Data/Application/77BFD79E-8A35-436C-B8F2-1E5CB1D047B1/Documents
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            string filePath = Path.Combine(path, "out.txt");
-
-            File.AppendAllText(filePath, $"{txtEntry.Text}#{date.Date + time.Time}\n");
-
-
-            //string text = File.ReadAllText(filePath);
-            //Console.WriteLine(text);
-
         }
     }
 }
