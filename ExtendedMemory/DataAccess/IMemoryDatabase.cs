6bd53285e6ec658bcc7efb5ce113b15d9982563c
Rohit <rohitnair987@gmail.com>
2018-02-25 20:42:55 -0500

    sqlLite installed and able to write and read to and from it.

---
+++ ExtendedMemory/DataAccess/IMemoryDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ExtendedMemory.Models;
+
+namespace ExtendedMemory.DataAccess
+{
+    public interface IMemoryDatabase
+    {
+        List<Memory> Get();
+
+        Memory Get(int id);
+
+        int Save(Memory item);
+
+        int Delete(Memory item);
+    }
+}
---
+++ iOS/DataAccess/MemoryDatabase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ExtendedMemory.DataAccess;
+using ExtendedMemory.Helpers;
+using ExtendedMemory.Models;
+using SQLite;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(MemoryDatabase))]
+namespace ExtendedMemory.DataAccess
+{
+    public class MemoryDatabase : IMemoryDatabase
+    {
+        static SQLiteConnection database;
+
+        public static SQLiteConnection Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new SQLiteConnection(DependencyService.Get<IFileLocation>().GetLocalFilePath("MemoryDatabase.db3"));
+                }
+                return database;
+            }
+        }
+
+        public MemoryDatabase()
+        {
+            database = new SQLiteConnection(DependencyService.Get<IFileLocation>().GetLocalFilePath("MemoryDatabase.db3"));
+            database.CreateTable<Memory>();
+        }
+
+        public int Save(Memory item)
+        {
+            if (item.ID != 0)
+            {
+                return database.Update(item);
+            }
+
+            return database.Insert(item);
+        }
+
+        public List<Memory> Get() => database.Table<Memory>().ToList();
+
+        public Memory Get(int id) => database.Table<Memory>().Where(i => i.ID == id).FirstOrDefault();
+
+        public int Delete(Memory item) => database.Delete(item);
+    }
+}
