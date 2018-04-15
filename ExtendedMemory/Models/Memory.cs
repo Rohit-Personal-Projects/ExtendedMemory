cebaecc687d31dcce19bb2d4f61558a054df9308
Rohit <rohitnair987@gmail.com>
2018-04-15 19:41:45 -0400

    Made the search results' listview as a ObservableCollection and made changes to make it update on the UI

--- ExtendedMemory/Models/Memory.cs
+++ ExtendedMemory/Models/Memory.cs
@@ -19,6 +19,33 @@ namespace ExtendedMemory.Models
 
         public DateTime DateTime { get; set; }
 
+        /// <summary>
+        /// Whether to show the details of the memory on the search results page.
+        /// </summary>
+        private bool ShowDetails;
+
+        /// <summary>
+        /// On the search results page, whether to display text or the entire memory.
+        /// </summary>
+        public string DisplayText;
+
+        /// <summary>
+        /// Switches what's displayed on the screen.
+        /// </summary>
+        public void ShowHideDetails()
+        {
+            if (ShowDetails)
+            {
+                ShowDetails = false;
+                DisplayText = Text;
+            }
+            else
+            {
+                ShowDetails = true;
+                DisplayText = StringRepresentation();
+            }
+        }
+
         public static Memory DictToMemory(QueryRow row)
         {
             var m = new Memory();
@@ -37,6 +64,8 @@ namespace ExtendedMemory.Models
                     {
                         case nameof(m.ID):
                         case nameof(m.Text):
+                        case nameof(m.DisplayText):
+                        case nameof(m.ShowDetails):
                             key.SetValue(result, val, null);
                             break;
 
@@ -62,11 +91,26 @@ namespace ExtendedMemory.Models
             return result;
         }
 
-        public override string ToString()
+        private string StringRepresentation()
         {
             return string.Format(
                 "[Memory: ID={0}, Text={1}, Date={2}, City={3}, State={4}, Country={5}, People=[{6}], Tags=[{7}]]", 
                 ID, Text, DateTime, Location?.City, Location?.State, Location?.Country, String.Join(", ", People), String.Join(", ", Tags));
+        }
+
+        public override string ToString()
+        {
+            return DisplayText;
+        }
+
+        public bool CustomEquals(Memory m)
+        {
+            return this.Text == m.Text &&
+                       this.ID == m.ID &&
+                       this.DateTime == m.DateTime &&
+                       this.Location == m.Location &&
+                       this.People == m.People &&
+                       this.Tags == m.Tags;
         }
     }
 }
