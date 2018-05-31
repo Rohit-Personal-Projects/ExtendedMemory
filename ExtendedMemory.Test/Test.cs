a5f05a3fb1acbaaf60911e45cc125ca670928de5
Rohit <rohitnair987@gmail.com>
2018-05-31 12:45:47 -0400

    buggy code to add tests to the project

--- ExtendedMemory.Test/Test.cs
+++ ExtendedMemory.Test/Test.cs
@@ -1,13 +1,17 @@
-﻿using NUnit.Framework;
+﻿using ExtendedMemory.DataAccess;
+using NUnit.Framework;
 using System;
 namespace ExtendedMemory.Test
 {
     [TestFixture()]
     public class Test
     {
-        [Test()]
+        [Test]
         public void TestCase()
         {
+            var m = new MemoryDatabase();
+            var c = m.Get().Result.Item.Count;
+            Assert.IsTrue(c > 0);
         }
     }
 }
